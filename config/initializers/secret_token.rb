dbe3ae41261e6a1072b8eb51433d08e7d5a52cd0
Bapu Goddanti <bapu.goddanti@gmail.com>
2011-09-30 09:51:41 -0700

    added a model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsdemo::Application.config.secret_token = 'cc8a2228e099c47d5365263bc755b4dac3487a2829dec46fc7a7220e39a44e9e95bba849d9464f118fb8030c98801100fe0efbbcda7854309db9c110c323856f'
